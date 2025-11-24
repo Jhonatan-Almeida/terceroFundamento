f1796ad1681260a65e0323977afa1ea7fa7db2f9
Jonathan Almeida <jhonalmeida@hotmail.com>
2025-11-24 18:49:26 -0500

    TRabajo de variables

---
+++ Variables.py
@@ -0,0 +1,14 @@
+entero = 10
+flotante = 3.14
+Texto = "Hola, Mundo"
+booleano = True
+
+
+print("Entero:", entero)
+print("Flotante:", flotante)
+print("Texto:", Texto)
+print("Booleano:", booleano)
+print("Entero:", type(entero))
+print("Flotante:", type(flotante))
+print("Texto:", type(Texto))
+print("Booleano:", type(booleano))
